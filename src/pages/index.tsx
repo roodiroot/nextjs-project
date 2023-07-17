728e5e1ddc33600157a3c677f2f9f3aec86aae45
Borisov Maxim <voodivood@gmail.com>
2023-07-17 23:26:00 +0300

    script

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,9 +32,12 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="yandex-verification" content="d35be75676e8516e" />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
+        <link rel="icon" href="/favicon2.ico" />
+      </Head>
+      <Script
+        id="my-script"
+        dangerouslySetInnerHTML={{
+          __html: `
                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
@@ -46,10 +49,8 @@ export default function Home() {
                       webvisor:true
                 });
               `,
-          }}
-        />
-        <link rel="icon" href="/favicon2.ico" />
-      </Head>
+        }}
+      />
       <div className={`pb-24`}>
         <MainBlockV2 scrollToGallery={scrollToGallery} />
         <Container>

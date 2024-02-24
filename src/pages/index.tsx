adb9026d55e64f1681851092f7f30377e74fc51a
Borisov Maxim <voodivood@gmail.com>
2024-02-24 13:55:09 +0300

    fix: 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,10 +32,11 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='yandex-verification' content='d35be75676e8516e' />
         <link rel='icon' href='/favicon2.ico' />
-        <Script
-          id='my-script'
-          dangerouslySetInnerHTML={{
-            __html: `
+      </Head>
+      <Script
+        id='my-script'
+        dangerouslySetInnerHTML={{
+          __html: `
                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
@@ -47,10 +48,8 @@ export default function Home() {
                       webvisor:true
                 });
               `,
-          }}
-        />
-      </Head>
-
+        }}
+      />
       <div className={`pb-24`}>
         <MainBlockV2 scrollToGallery={scrollToGallery} />
         <Container>

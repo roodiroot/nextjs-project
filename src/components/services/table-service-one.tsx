0b397ff1079f294454a23fd8d547e3a3580b2e74
RoodiRoot <borisov130490@gmail.com>
2024-03-04 15:57:34 +0300

    change price

--- src/components/services/table-service-one.tsx
+++ src/components/services/table-service-one.tsx
@@ -3,15 +3,15 @@ import Heading from "../Heading";
 const TableServiceOne = () => {
   return (
     <div
-      className='
+      className="
             pb-12
             w-full
             flex
             flex-col
             gap-6
-            '
+            "
     >
-      <Heading h1 title='Стоимость установки приборов' />
+      <Heading h1 title="Стоимость установки приборов" />
       <table>
         <tbody>
           <tr>
@@ -19,48 +19,48 @@ const TableServiceOne = () => {
             <th>Модели</th>
             <th>Цена*</th>
           </tr>
-          <tr className='bg-white even:bg-gray-100'>
-            <td className='p-2 '>
+          <tr className="bg-white even:bg-gray-100">
+            <td className="p-2 ">
               <h2>Закладка магистрали до 5м</h2>
             </td>
-            <td className='p-2 font-semibold'>07-09 BTU или до 26м²</td>
-            <td className='p-2 '>14000 руб.</td>
+            <td className="p-2 font-semibold">07-09 BTU или до 26м²</td>
+            <td className="p-2 ">15000 руб.</td>
           </tr>
-          <tr className='bg-white even:bg-gray-100'>
-            <td className='p-2 '>
+          <tr className="bg-white even:bg-gray-100">
+            <td className="p-2 ">
               <h2>Закладка магистрали до 5м</h2>
             </td>
-            <td className='p-2 font-semibold'>12 BTU или до 35м²</td>
-            <td className='p-2 '>15000 руб.</td>
+            <td className="p-2 font-semibold">12 BTU или до 35м²</td>
+            <td className="p-2 ">16000 руб.</td>
           </tr>
-          <tr className='bg-white even:bg-gray-100'>
-            <td className='p-2 '>
+          <tr className="bg-white even:bg-gray-100">
+            <td className="p-2 ">
               <h2>Закладка магистрали до 5м</h2>
             </td>
-            <td className='p-2 font-semibold'>18 BTU или до 52м²</td>
-            <td className='p-2 '>17000 руб.</td>
+            <td className="p-2 font-semibold">18 BTU или до 52м²</td>
+            <td className="p-2 ">17000 руб.</td>
           </tr>
-          <tr className='bg-white even:bg-gray-100'>
-            <td className='p-2 '>
+          <tr className="bg-white even:bg-gray-100">
+            <td className="p-2 ">
               <h2>Закладка магистрали до 5м</h2>
             </td>
-            <td className='p-2 font-semibold'>24 BTU или до 70м²</td>
-            <td className='p-2 '>20000 руб.</td>
+            <td className="p-2 font-semibold">24 BTU или до 70м²</td>
+            <td className="p-2 ">20000 руб.</td>
           </tr>
         </tbody>
       </table>
-      <div className='w-full text-base text-slate-900'>
+      <div className="w-full text-base text-slate-900">
         Для консультации или заказа установки звоните по телефонам{" "}
-        <a className='text-orange-500 whitespace-nowrap' href='tel:89153294209'>
+        <a className="text-orange-500 whitespace-nowrap" href="tel:89153294209">
           8 (915) 329-42-09
         </a>
         ,{" "}
-        <a className='text-orange-500 whitespace-nowrap' href='tel:84956752699'>
+        <a className="text-orange-500 whitespace-nowrap" href="tel:84956752699">
           8 (495) 675-26-99
-          <span className='text-sm text-slate-900'> c 9:00 до 21:00</span>
+          <span className="text-sm text-slate-900"> c 9:00 до 21:00</span>
         </a>
       </div>
-      <div className='w-full text-xs text-slate-500'>
+      <div className="w-full text-xs text-slate-500">
         *Обращаем внимание на то что цены указаны с учетом преобретения техники
         у нас в магазине. Если прибор куплен в другом месте цена за установку
         может незначительно меняться. Это связано с тем что технически приборы

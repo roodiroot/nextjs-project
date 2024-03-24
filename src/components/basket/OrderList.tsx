f8f8f93ad77c236d2113d1aef40f1a5b52405f7c
RoodiRoot <borisov130490@gmail.com>
2024-03-24 18:06:30 +0300

    fix: change price service

--- src/components/basket/OrderList.tsx
+++ src/components/basket/OrderList.tsx
@@ -21,7 +21,7 @@ const OrderList: React.FC<OrderProps> = ({
   let total = summ;
 
   if (installation) {
-    total += 14000;
+    total += 15000;
   }
   if (distance) {
     total += 30 * distance;
@@ -35,30 +35,30 @@ const OrderList: React.FC<OrderProps> = ({
   }, [total, distance, installation, setTotal]);
 
   return (
-    <div className='flex-none w-full bg-slate-100 rounded-md md:max-w-[315px] h-auto p-8'>
-      <div className='text-xl font-medium text-slate-900 mb-3'>В корзине</div>
-      <div className='flex flex-row justify-between mb-1'>
-        <div className='text-zinc-500'>{count} товар(a):</div>
-        <div className='text-slate-900 font-semibold'>{summ} руб.</div>
+    <div className="flex-none w-full bg-slate-100 rounded-md md:max-w-[315px] h-auto p-8">
+      <div className="text-xl font-medium text-slate-900 mb-3">В корзине</div>
+      <div className="flex flex-row justify-between mb-1">
+        <div className="text-zinc-500">{count} товар(a):</div>
+        <div className="text-slate-900 font-semibold">{summ} руб.</div>
       </div>
-      <div className='flex flex-row justify-between mb-1'>
-        <div className='text-zinc-500'>Доставка:</div>
-        <div className='text-slate-900 font-semibold'>
+      <div className="flex flex-row justify-between mb-1">
+        <div className="text-zinc-500">Доставка:</div>
+        <div className="text-slate-900 font-semibold">
           {distance ? `${distance}км. х 30руб*.` : "0 руб*."}
         </div>
       </div>
-      <div className='flex flex-row justify-between mb-3 border-b pb-2'>
-        <div className='text-zinc-500'>Установка:</div>
-        <div className='text-slate-900 font-semibold'>
+      <div className="flex flex-row justify-between mb-3 border-b pb-2">
+        <div className="text-zinc-500">Установка:</div>
+        <div className="text-slate-900 font-semibold">
           {installation ? "14000" : "0"} руб*.
         </div>
       </div>
-      <div className='flex flex-row justify-between mb-3'>
-        <div className='text-slate-900 font-semibold '>Итого:</div>
-        <div className='text-slate-900 font-semibold text-lg'>{total} руб.</div>
+      <div className="flex flex-row justify-between mb-3">
+        <div className="text-slate-900 font-semibold ">Итого:</div>
+        <div className="text-slate-900 font-semibold text-lg">{total} руб.</div>
       </div>
-      <Button onClick={getOrder} label='Оформить заказ' />
-      <div className='text-sm mt-2'>
+      <Button onClick={getOrder} label="Оформить заказ" />
+      <div className="text-sm mt-2">
         *Стоимость доставки и установки может меняться после уточнения деталей
         заказа
       </div>

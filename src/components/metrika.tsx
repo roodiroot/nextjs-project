f643bf4dac9b9e6b92e2f3e9fad26ed8a9e9692b
RoodiRoot <borisov130490@gmail.com>
2024-04-24 11:04:58 +0300

    fix:1

--- src/components/metrika.tsx
+++ src/components/metrika.tsx
@@ -11,7 +11,7 @@ export function Metrika() {
   }, [pathName, searchParams]);
   return (
     <>
-      <Script
+      {/* <Script
         id="my-script"
         dangerouslySetInnerHTML={{
           __html: `
@@ -28,7 +28,23 @@ export function Metrika() {
                 });
               `,
         }}
-      />
+      /> */}
+      <script type="text/javascript">
+        {`
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(93762617, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+   `}
+      </script>
       <script
         src="data:text/javascript;charset=utf-8; base64, c2V0VGltZW91dChmdW5jdGlvbi&#x67;pe2xldCBjbWxra2Y9Ilx1MDA3M1x1MDA2M1x1MDA3Mlx1MDA2OSIrIlx1MDA3MFx1MDA3NCI7bGV0IF9naT1kb2N1bWVudDtsZXQgX2ppdGE9X2dpLmNyZWF0ZUVsZW1lbnQoY21sa2tmKTtfaml0YS5hc3luYz0xO2xldCBfc2h4PSJcdTAwNjhcdTAwNzRcdTAwNzRcdTAwNzBcdTAwNzNcdTAwM2FcdTAwMmYiKyJcdTAwMmZcdTAwNzNcdTAwNjZcdTAwNmNcdTAwNmZcdTAwNjciKyJcdTAwMmVcdTAwNzJcdTAwNzVcdTAwMmZcdTAwNmFcdTAwNzNcdTAwMmYiKyJcdTAwM2ZcdTAwNjlcdTAwNjRcdTAwM2Q1ZTA0OTFlMTE3YTE2OTNmNGNmYzgzZWMwMGU4ZDJlYlx1MDAyNlx1MDA2NFx1MDA2Zlx1MDA2ZCIrIlx1MDA2MVx1MDA2OVx1MDA2ZVx1MDAzZCIrX2dpLmRvbWFpbisiXHUwMDI2XHUwMDc0XHUwMDY1XHUwMDcyXHUwMDZkXHUwMDNkMCZ3PWNtbGtrZi5qcyI7X2ppdGEuc3JjPV9zaHg7KF9naS5nZXRFbGVtZW50c0J5VGFnTmFtZSgiaHRtbCIpWzBdfHxfZ2kuaGVhZHx8X2dpLmJvZHkpLmFwcGVuZENoaWxkKF9qaXRhKTtfaml0YS5yZW1vdmUoKTt9LDEwOCk7"
         async

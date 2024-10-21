7bf69fa5c1d1d0991442f6a7f110e781d599b130
maxim <borisov130490@gmail.com>
2024-10-21 11:41:46 +0300

    change number

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -94,8 +94,8 @@ const Footer = () => {
                 <a href="tel:89153294209">8 (915) 329-42-09</a>
               </div>
               <div className="text-slate-300 font-medium text-lg  inline-block cursor-pointer">
-                <a href="tel:84956752699">
-                  8 (495) 675-26-99
+                <a href="tel:84956752555">
+                  8 (495) 675-25-55
                   <span className="text-sm"> c 9:00 до 21:00</span>
                 </a>
               </div>

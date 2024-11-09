f716a6964fa54468464fd774d2b466a0a00ca5db
maxim <borisov130490@gmail.com>
2024-11-09 09:21:29 +0300

    fix:1

--- src/components/BotControl.tsx
+++ src/components/BotControl.tsx
@@ -1,8 +1,10 @@
 export function BotControl() {
   return (
     <>
-      <script type="text/javascript">window._ab_id_=149815</script>
-      <script src="https://cdn.botfaqtor.ru/one.js"></script>
+      <script type="text/javascript" defer>
+        window._ab_id_=149815
+      </script>
+      <script src="https://cdn.botfaqtor.ru/one.js" defer></script>
     </>
   );
 }
